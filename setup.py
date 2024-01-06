fe1d19fc65bb7b7a42eb9685fc728fdc7a7c14ca
dresis <147135495+dresis@users.noreply.github.com>
2024-01-06 12:53:31 -0700

    Update setup.py
    Changed version number due to past errors with upload to PyPi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 s = setup(
     name='QtMPLtools',
-    version='0.0.1',
+    version='1.0.0',
     description='Matplotlib plugins for Qt designer',
     long_description=long_description,
     long_description_content_type="text/markdown",
